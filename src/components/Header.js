93c9eadf116217f41ad9f96b5bdfaeb1ff6f21b3
ryusufu <34818816+ryusufu@users.noreply.github.com>
2020-07-05 23:56:20 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../Context";
+
+function Header() {
+  const { cartItems } = useContext(Context);
+  const cartClassName =
+    cartItems.length > 0 ? (
+      <i className="ri-shopping-cart-fill ri-fw ri-2x" />
+    ) : (
+      <i className="ri-shopping-cart-line ri-fw ri-2x" />
+    );
+  return (
+    <header>
+      <Link to="/">
+        {" "}
+        <h2>Pic Some</h2>{" "}
+      </Link>
+      <Link to="/cart">
+        {/* <i className="gg-shopping-cart" /> */}
+        {cartClassName}
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Cart.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
+function Cart() {
+  const { cartItems } = useContext(Context);
+  const total = 5.99 * cartItems.length;
+  const totalCostDisplay = total.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
+  const cartItemElements = cartItems.map(item => (
+    <CartItem key={item.id} item={item} />
+  ));
+  return (
+    <main className="cart-page">
+      <h1>Check out</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: {totalCostDisplay} </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
+    </main>
+  );
+}
+
+export default Cart;
